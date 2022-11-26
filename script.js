c07df13df0b3b5b9c8f1a39220912b27f84c23e2
Kushagra Kukreti <kukretikushagra@gmail.com>
2022-11-26 22:53:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let taskbar = document.getElementsByClassName("taskbar")[0];
+let startmenu = document.getElementsByClassName("startmenu")[0];
+taskbar.addEventListener("click", () => {
+    if (startmenu.style.bottom == "-70px") {
+        startmenu.style.bottom = "-632px"
+    }
+    else {
+        startmenu.style.bottom = "-70px"
+    }
+})
